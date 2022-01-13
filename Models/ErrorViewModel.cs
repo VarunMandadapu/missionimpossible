12954662d1a3b95bf1ebda4ed917f23cc556c334
Amith <solvedbyamith@gmail.com>
2022-01-13 18:02:35 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace missionimpossible.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
